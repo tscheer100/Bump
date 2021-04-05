41400b7e7f8e933a27167ff40c3ee26d2bd88263
Thomas Scheer <thomas.k.scheer@gmail.com>
2021-04-05 13:45:53 -0500

    rewrote major function, added readme

--- bump.py
+++ bump.py
@@ -7,10 +7,11 @@ import asyncio
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
+import time
 
 
 # uncomment the line below if you're trying to edit code in VSCode
-# os.chdir('./Bump')
+os.chdir('./Bump')
 load_dotenv('./.env')
 DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
 
@@ -22,33 +23,53 @@ client = commands.Bot(intents = intents, command_prefix = "!t ", status=discord.
 async def on_ready():
     print("Bump Reminder is now running")
 
+# @client.event
+# async def on_message(message):
+#     ch = message.channel
+
+#     if message.author.id == 302050872383242240: # <---- this number is the Disboard user ID
+
+#         await ch.send("bump called")
+#         if message.embeds:
+#             await ch.send("embed")
+
+#             if "done" in message.embeds[0].description:
+#                 await ch.send("bump success, starting timer for 120 minutes")
+#                 await asyncio.sleep(7200)
+#                 await ch.send("Time to bump.")
+#             elif "wait" in message.embeds[0].description:
+#                 desc = message.embeds[0].description
+#                 num = [int(i) for i in desc.split() if i.isdigit()]
+#                 num_conv = str(num[0])
+#                 print(num)
+#                 await ch.send("bump wait " + num_conv + " minutes")
+#                 await asyncio.sleep(num[0] * 60)
+#                 await ch.send("time to bump again!")
+#             else:
+#                 await ch.send("desc uncaught")
+#                 print("nothing found")
+#     await client.process_commands(message)
+
 @client.event
 async def on_message(message):
     ch = message.channel
+    time = 0
 
-    if message.author.id == 302050872383242240: # <---- this number is the Disboard user ID
-
-        await ch.send("bump called")
+    if message.author.id == 302050872383242240:
         if message.embeds:
-            await ch.send("embed")
-
+            await ch.send('embed')
             if "done" in message.embeds[0].description:
-                await ch.send("bump success, starting timer for 120 minutes")
-                await asyncio.sleep(7200)
-                await ch.send("Time to bump.")
+                time = 120
             elif "wait" in message.embeds[0].description:
                 desc = message.embeds[0].description
-                num = [int(i) for i in desc.split() if i.isdigit()]
-                num_conv = str(num[0])
-                print(num)
-                await ch.send("bump wait " + num_conv + " minutes")
-                await asyncio.sleep(num[0] * 60)
-                await ch.send("time to bump again!")
+                time_list = [int(i) for i in desc.split() if i.isdigit()]
+                time = time_list[0]
+                time_conv = str(time_list[0])
+                await ch.send(str(time)+ " asdfasdf") # debug check
+                await ch.send("bump again in+ " + time_conv + " minutes")
             else:
                 await ch.send("desc uncaught")
-                print("nothing found")
-    await client.process_commands(message)
+        await asyncio.sleep(time * 60)
+        await ch.send("time to bump! waited " + str(time * 60))
 
-
-    
 client.run(DISCORD_TOKEN)
---
+++ readme.md
@@ -0,0 +1,10 @@
+# Bump Bot
+A discord bot created to remind users to bump the server 2 hours after the previous bump.
+
+## Requirements:
+- Python 3.X
+- [Discord.py](https://pypi.org/project/discord.py/)
+- [Dotenv](https://pypi.org/project/python-dotenv/)
+
+### What I've learned from this project
+- the benefits of using `asyncio` over `time.sleep`
