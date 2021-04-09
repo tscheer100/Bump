588456c07e3553e07e8e57e60419b9b4ba87ab24
Thomas Scheer <thomas.k.scheer@gmail.com>
2021-04-08 22:04:19 -0500

    added embeds to main function

--- bump.py
+++ bump.py
@@ -17,7 +17,7 @@ DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
 
 intents = discord.Intents(members = True, messages = True, guilds = True)
 
-client = commands.Bot(intents = intents, command_prefix = "!t ", status=discord.Status.online, activity=discord.Game("under construction"))
+client = commands.Bot(intents = intents, command_prefix = "!", status=discord.Status.online, activity=discord.Game("under construction"))
 
 bumping = False
 
@@ -39,22 +39,35 @@ async def on_message(message):
             await lock.acquire()
             try:
                 if "done" in message.embeds[0].description:
+                    # done statement still needs to be checked.
                     time = 120
+                    embed_done = discord.Embed(
+                        title = "Bump complete!",
+                        color = discord.Colour.purple(),
+                        description = "bump again in 2 hours"
+                    )
+                    await ch.send(embed = embed_done)
                 elif "wait" in message.embeds[0].description:
                     desc = message.embeds[0].description
                     time_list = [int(i) for i in desc.split() if i.isdigit()]
                     time = time_list[0]
                     time_conv = str(time_list[0])
-                    await ch.send("bump again in " + time_conv + " minutes")
+
+                    embed_wait = discord.Embed(
+                        title = "Wait to bump",
+                        color = discord.Colour.purple(),
+                        description = "Bump again in " + time_conv + " minutes"
+                    )       
+                    await ch.send(embed = embed_wait)
                 else:
-                    await ch.send("desc uncaught") # debug
+                    print("desc uncaught") # debug
             finally:
                 if bumping == False:
                     should_bump = True
                     bumping = True
                 lock.release()
                 if should_bump:
-                    await asyncio.sleep(time * 60) # time shortened for debug
+                    await asyncio.sleep(time * .060) # time shortened for debug
                     await ch.send("time to bump! waited " + str(time))
                     bumping = False
 
